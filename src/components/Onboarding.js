784f3bee7536af6f43e574f147f2a2f1fc603d49
Nguyễn Hữu Toàn <mac@huutoan.domain.com>
2022-02-08 23:54:07 +0700

    Fix ref of onboarding

--- src/components/Onboarding.js
+++ src/components/Onboarding.js
@@ -1,18 +1,51 @@
-import React from "react";
-import { View, Text, StyleSheet, FlatList } from "react-native";
+import React, { useState, useRef } from "react";
+import { View, Text, StyleSheet, FlatList, Animated } from "react-native";
 import slides from "../common/slide";
 
 import OnboardingItem from "../constants/OnboardingItem";
 
 const Onboarding = () => {
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  // Get positon of list
+  const scrollX = useRef(new Animated.Value(0)).current;
+  const slidesRef = useRef(null);
+
+  const viewableItemChaged = useRef(({ viewableItems }) => {
+    setCurrentIndex(viewableItems[0].index);
+  }).current;
+
+  const viewCofig = useRef({ viewAreaCoveragePercentThreshold: 50 }).current;
+
   return (
     <View style={styles.container}>
-      <FlatList
-        data={slides}
-        renderItem={({ item }) => <OnboardingItem item={item} />}
-        horizontal
-        showsHorizontalScrollIndicator
-      />
+      <View style={{ flex: 3 }}>
+        <FlatList
+          data={slides}
+          renderItem={({ item }) => <OnboardingItem item={item} />}
+          horizontal
+          showsHorizontalScrollIndicator
+          pagingEnabled
+          bounces={false}
+          keyExtractor={(item) => item.id}
+          onScroll={Animated.event(
+            [
+              {
+                nativeEvent: {
+                  contentOffset: {
+                    x: scrollX,
+                  },
+                },
+              },
+            ],
+            { useNativeDriver: false }
+          )}
+          scrollEventThrottle={32}
+          onViewableItemsChanged={viewableItemChaged}
+          viewabilityConfig={viewCofig}
+          ref={slidesRef}
+        />
+      </View>
     </View>
   );
 };
